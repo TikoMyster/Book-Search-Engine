544c4247a6d606f1204b6070245aead9ee334b95
TikoMyster <tikomyster20@gmail.com>
2023-02-13 18:28:35 -0800

    updated server.js and readme

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -6,16 +6,26 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const __dirname = path.dirname("");
+
+app.use(express.static(path.join(__dirname, "../client/public/index.html")))
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/public/index.html')));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"))
+})
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    
+});
 });
